b4b64704daa79b9fcb0ab9476c5aafa2c036833d
Tope Ojo <topedigit@gmail.com>
2026-02-26 01:46:25 +0100

    Added db-test route

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+import os
 
 app = FastAPI()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
 @app.get("/")
 def home():
-    return {"message": "Backend is working!"}
+    return {"message": "Backend is working with DB!"}
+
+@app.get("/db-test")
+def db_test():
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT 1"))
+        return {"database_response": str(result.scalar())}
